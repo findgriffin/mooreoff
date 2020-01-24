86d272b64ede47e4ef9afa74c3866f1ff55f626e
David Griffin <david.griffin@blinkhealth.com>
2020-01-24 12:43:06 -0800

    Fix units and make running for n hours per day a first class concept.

--- mooreoff/monte_carlo_requests.py
+++ mooreoff/monte_carlo_requests.py
@@ -5,17 +5,15 @@ import random
 from itertools import repeat
 
 
-MS_PER_SEC = 1000
-SEC_PER_MIN = 60
-MIN_PER_HR = 60
-HR_PER_DAY = 1
 DURATION_MS = 100
-
-MIN_PER_DAY = MIN_PER_HR * HR_PER_DAY
-SEC_PER_DAY = MIN_PER_DAY * SEC_PER_MIN
-MS_PER_DAY = SEC_PER_DAY * MS_PER_SEC
-
+MIN_PER_HR = 60
+MS_PER_SEC = 1000
+HR_PER_DAY = 24
+SEC_PER_MIN = 60
+SIMULATION_HOURS = 1
 PERCENT = 100
+
+
 PERCENTILES = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99, 99.9, 99.99, 99.999, 100]
 
 
@@ -43,8 +41,8 @@ def run_insert(duration, buckets, requests):
 def sort_buckets(buckets):
     buckets.sort()
 
-def print_csv_line(collection, prefix=''):
-    print(",\t".join([f"{prefix}{i}" for i in collection]))
+def print_csv_line(collection, row, prefix=''):
+    print(f"{row}\t" + ",\t".join([f"{prefix}{i}" for i in collection]))
     
 
 
@@ -52,13 +50,14 @@ def bucket_for_percentile(percentile, bucket_count):
     return min(int(bucket_count*percentile/PERCENT), bucket_count-1)
 
 @timeit
-def run_monte_carlo(duration_in_buckets, bucket_count, daily_traffic):
-    PART_1 = f"Monte Carlo: {bucket_count} buckets, "
-    PART_2 = f"{duration_in_buckets} buckets request duration, "
-    PART_3 = f"and {daily_traffic} requests per day "
+def run_monte_carlo(duration_in_ms, daily_traffic):
+    bucket_count = MS_PER_SEC * SEC_PER_MIN * MIN_PER_HR * SIMULATION_HOURS
+    PART_1 = f"Monte Carlo: {SIMULATION_HOURS} hours, "
+    PART_2 = f"{duration_in_ms} ms request duration, "
+    PART_3 = f"and {'{:,}'.format(daily_traffic)} requests per day. "
     print(PART_1 + PART_2 + PART_3)
     buckets = [0] * bucket_count
-    run_insert(duration_in_buckets, buckets, daily_traffic)
+    run_insert(duration_in_ms, buckets, int(daily_traffic / SIMULATION_HOURS / HR_PER_DAY))
     sort_buckets(buckets)
     percentiles = [] 
     for percentile in PERCENTILES:
@@ -67,15 +66,17 @@ def run_monte_carlo(duration_in_buckets, bucket_count, daily_traffic):
     return percentiles
 
 
-result_1k = run_monte_carlo(DURATION_MS, MS_PER_DAY, int(1_000/24))
-result_10k = run_monte_carlo(DURATION_MS, MS_PER_DAY, int(10_000/24))
-result_100k = run_monte_carlo(DURATION_MS, MS_PER_DAY, int(100_000/24))
-result_1MM = run_monte_carlo(DURATION_MS, MS_PER_DAY, int(1_000_000/24))
-result_10MM = run_monte_carlo(DURATION_MS, MS_PER_DAY, int(10_000_000/24))
-print_csv_line(PERCENTILES, prefix='p')
-print_csv_line(result_1k)
-print_csv_line(result_10k)
-print_csv_line(result_100k)
-print_csv_line(result_1MM)
-print_csv_line(result_10MM)
-
+print(f"Simulating {SIMULATION_HOURS} hours of one day.")
+result_1k = run_monte_carlo(DURATION_MS, 1_000)
+result_10k = run_monte_carlo(DURATION_MS, 10_000)
+result_100k = run_monte_carlo(DURATION_MS, 100_000)
+result_1MM = run_monte_carlo(DURATION_MS, 1_000_000)
+result_10MM = run_monte_carlo(DURATION_MS, 10_000_000)
+#result_100MM = run_monte_carlo(DURATION_MS, 100_000_000)
+print_csv_line(PERCENTILES, "", prefix='p')
+print_csv_line(result_1k, "1k")
+print_csv_line(result_10k, "10k")
+print_csv_line(result_100k, "100k")
+print_csv_line(result_1MM, "1MM")
+print_csv_line(result_10MM, "10MM")
+#print_csv_line(result_100MM, "100MM")
