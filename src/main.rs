fdf5092368e56e1638ef66b0c1fc6401eca1a639
David Griffin <findgriffin@gmail.com>
2020-02-03 00:02:05 -0800

    Use Rust random number generator.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+extern crate rand;
+
  
 fn insert_one_req(buckets_per_req: u32, bucket_array: &mut [u32]) {
     let random = 3;
-    let start = random;
+    let start = rand::random::<u32>();
     for index in start..start + buckets_per_req {
         bucket_array[index as usize % bucket_array.len()] += 1;
     }
 }
 
 fn main() {
+    let mut rng = rand::thread_rng();
     let mut bucket_array: [u32; 5] = [0, 0, 0, 0, 0];
+    println!("Array: {:?}", &bucket_array);
     insert_one_req(4, &mut bucket_array);
-    println!("Hello, world!");
+    println!("Array: {:?}", &bucket_array);
 }
