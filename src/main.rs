ccc292ff88cd329db5280d9fd63fe9bfc84b614b
David Griffin <findgriffin@gmail.com>
2020-02-04 00:28:53 -0800

    Implement Monte Carlo simulation in Rust.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 extern crate rand;
 
+const MS_PER_SEC: u32 = 1000;
+const SEC_PER_MIN: u32 = 60;
+const MIN_PER_HR: u32 = 60;
+const SIMULATION_HOURS: u32 = 1;
  
 fn insert_one_req(duration: u32, buckets: &mut [u32]) {
     let start = rand::random::<u32>();
@@ -15,10 +19,18 @@ fn insert_reqs(duration: u32, buckets: &mut [u32], requests: u32) {
     }
 }
 
+fn run_monte_carlo(duration: u32, daily_traffic: u32) {
+    const bucket_count: u32 = MS_PER_SEC * SEC_PER_MIN * MIN_PER_HR * SIMULATION_HOURS;
+    print!("Monte Carlo: {} hours, ", SIMULATION_HOURS);
+    print!("{} ms request duration, ", duration);
+    print!("and {} requests per day. ", daily_traffic);
+    let mut bucket_array = vec![0; bucket_count as usize];
+    insert_reqs(duration, &mut bucket_array, daily_traffic);
+    bucket_array.sort();
+    println!("Highest value is {}", bucket_array[bucket_array.len()-1]);
+}
+
 
 fn main() {
-    let mut bucket_array: [u32; 12] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    println!("Array: {:?}", &bucket_array);
-    insert_reqs(4, &mut bucket_array, 10);
-    println!("Array: {:?}", &bucket_array);
+    run_monte_carlo(10, 10000000);
 }
